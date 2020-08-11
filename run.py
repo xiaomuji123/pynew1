408fb3b24ea3cb44f90d63eb00fb9ae29739412e
xiaomuji123 <68993825+xiaomuji123@users.noreply.github.com>
2020-08-11 12:35:06 +0800

    Update run.py

--- run.py
+++ run.py
@@ -37,69 +37,8 @@ def reptile():         #爬取新闻头条
             f.write(str(item))
             f.write('\n')
     print('.................爬虫结束...............')
-    mail(file)
-
-def mail(file):                   #发送邮件
-    #163邮箱服务器地址
-    mail_host = "mail.yy.com"
-    mail_user = "yy-monkeytest@yy.com"
-    mail_pass = "Yy1233211234567"
-    #邮件发送方邮箱地址
-    sender = 'yy-monkeytest@yy.com'
-    #邮件接受方邮箱地址，注意需要[]包裹，这意味着你可以写多个邮件地址群发
-    receiver = ['chenhaihe@yy.com']
-
-    #设置eamil信息
-    #添加一个MIMEmultipart类，处理正文及附件
-    message = MIMEMultipart()
-    #邮件主题
-    message['Subject'] = '每天爬虫日报'
-    #发送方信息
-    message['From'] = sender
-    #接受方信息
-    message['To'] = receiver[0]
-
-    #推荐使用html格式的正文内容，这样比较灵活，可以附加图片地址，调整格式等
-    # with open('abc.html','r') as f:
-    #     content1 = f.read()
-    #设置html格式参数（即正文内容）
-    content1 = '您的宝贝已到达，请查看附件'
-    part1 = MIMEText(content1,'html','utf-8')
-
-    #添加一个txt文本附件
-    with open(file,'r')as h:
-        content2 = h.read()
-    #设置txt参数
-    part2 = MIMEText(content2,'plain','utf-8')
-    #附件设置内容类型，方便起见，设置为二进制流
-    part2['Content-Type'] = 'application/octet-stream'
-    #设置附件头，添加文件名
-    part2.add_header('Content-Disposition', 'attachment', filename=file)
-
-    message.attach(part1)
-    message.attach(part2)
-
-    try:
-        server = smtplib.SMTP()
-        # 连接到服务器
-        server.connect(mail_host)
-        # 登录到服务器
-        server.login(mail_user, mail_pass)
-        # 发送
-        server.sendmail(sender, receiver, message.as_string())
-        # 退出
-        server.close()
-        print('邮件发送成功')
-    except Exception as e:
-        print('email exception', str(e))
-
-# def main(h=14, m=23):         #定时爬取
-#     while True:
-#         now = datetime.datetime.now()
-#         # print(now.hour, now.minute)
-#         if now.hour == h and now.minute == m:
-#             reptile()
-#         # 每隔60秒检测一次
-#         time.sleep(60)
-
-reptile()
+    
+if __name__ == '__main__':
+    print("走你")
+    reptile()
+    print("搞完收工~")
